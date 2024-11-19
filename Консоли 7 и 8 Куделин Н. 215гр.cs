3d569f8bcb6af1c639d32db8af11e8578f6c8a7a
KudelinNikita <Rokotanskiy8@gmail.com>
2024-11-19 13:19:48 +0200

    Add files via upload

---
+++ Консоли 7 и 8 Куделин Н. 215гр.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Консоль_7_Куделин_Н._215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ПРАКТИКУМ 7
+
+            //Задание 1 практикум 7
+
+            //Console.Write("Введите слово");
+            //string st = Console.ReadLine();
+            //Console.Write("n =");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int count = 1; count <= n; count++)
+            //{
+            //    Console.WriteLine(st);
+            //}
+            //Console.Read();
+
+
+            //ПРАКТИКУМ 8(ЗАВЕРШЁН)
+
+            //Задание 1 практикум 8
+            //Заполнить одномерный массив из 8 целых чисел с клавиатуры и вывести его на экран.
+
+            //int[] omas = new int[8];
+            //Console.WriteLine("Заполнение");
+            //for (int i=0;i<8;i++)
+            //{
+            //    Console.Write($"omas[{i}]=");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("Вывод о.м.");
+            //    for (int i = 0; i < 8; i++)  
+            //{
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //Console.Read();
+
+            //Задание 2 практикум 8
+            //Составьте программу ввода(без клавиатуры) и вывода массива: 5, -2, 3, -6, 7, 9, -4, 8, 1, 2, 7, -3.
+
+            //int[] omas = new int[12] { 5, -2, 3, -6, 7, 9, -4, 8, 1, 2, 7, -3 };
+            //Console.WriteLine("Вывод о.м.");
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //Console.Read();
+
+            //Задание 3 практикума 8
+            //Массив предназначен для хранения значений ростов двенадцати человек.
+            //С помощью датчика случайных чисел заполнить массив целыми значениями,
+            //лежащими в диапазоне от 163 до 190 включительно.
+
+            //int[] omas = new int[12];
+            //Random rnd = new Random();
+            //for (int i=0;i<12;i++)
+            //{
+            //    omas[i] = rnd.Next(163, 190);
+            //}
+            //foreach (var element in omas)
+            //{
+            //    Console.Write($"{element}\t");
+            //}
+            //Console.Read();
+        }
+    }
+}
